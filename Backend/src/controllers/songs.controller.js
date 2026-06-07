064cfd356ae66ea8bc7e3646239b6ac263efc0d1
Kirtanyadav199 <kirtanyadav24@gmail.com>
2026-06-07 12:41:36 +0530

    feat: implement getSong API

--- Backend/src/controllers/songs.controller.js
+++ Backend/src/controllers/songs.controller.js
@@ -1,51 +1,57 @@
-const songModel = require("../models/song.model")
-const asyncHandler = require('express-async-handler')
-const id3 = require('node-id3')
-const storageService = require('../services/storage.service')
+const songModel = require("../models/song.model");
+const asyncHandler = require("express-async-handler");
+const id3 = require("node-id3");
+const storageService = require("../services/storage.service");
 
-const uploadSong = asyncHandler(async (req,res)=>{
-    console.log("req recevied");
-    
-    const songBuffer = req.file.buffer
-    console.log("song recieved");
-    
-    const {mood} = req.body
-    console.log("mood recieved");
-    
-    const tag = id3.read(songBuffer)
-    
-    const songFile = await storageService.uploadFile({
-        buffer:songBuffer,
-        fileName:tag.title+".mp3",
-        folder:"/cohort-2/AuraPlay/songs"
-    })
-        console.log("song uploaded")
-    const posterFile = await storageService.uploadFile({
-        buffer:tag.image.imageBuffer,
-        fileName:tag.title+".jpeg",
-        folder:"/cohort-2/AuraPlay/posters"
-    })
-    console.log("poster uploaded")
+const uploadSong = asyncHandler(async (req, res) => {
+  const songBuffer = req.file.buffer;
 
-    const song = await songModel.create({
-        url:songFile.url,
-        posterUrl:posterFile.url,
-        title:tag.title,
-        mood
-    })
+  const { mood } = req.body;
 
-    console.log("song created")
+  const tag = id3.read(songBuffer);
 
-    res.status(201).json({
-        message:"song created successfully",
-        song
-    })
+  console.log("Starting song upload");
+  const [songFile, posterFile] = await Promise.all([
+    storageService.uploadFile({
+      buffer: songBuffer,
+      fileName: tag.title + ".mp3",
+      folder: "/cohort-2/AuraPlay/songs",
+    }),
 
+    storageService.uploadFile({
+      buffer: tag.image.imageBuffer,
+      fileName: tag.title + ".jpeg",
+      folder: "/cohort-2/AuraPlay/posters",
+    }),
+  ]);
 
-    
-})
+  const song = await songModel.create({
+    url: songFile.url,
+    posterUrl: posterFile.url,
+    title: tag.title,
+    mood,
+  });
 
+  res.status(201).json({
+    message: "song created successfully",
+    song,
+  });
+});
+
+const getSong = asyncHandler(async (req, res) => {
+  const { mood } = req.query;
+
+  const song = await songModel.findOne({
+    mood,
+  });
+
+  res.status(200).json({
+    message: "song fetched successfully",
+    song,
+  });
+});
 
 module.exports = {
-    uploadSong
-}
+  uploadSong,
+  getSong
+};
